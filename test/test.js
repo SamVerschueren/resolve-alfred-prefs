1d51e2cdb2c4507ec6537289da6215b9e21a6643
Jakob Krigovsky <jakob@krigovsky.com>
2019-03-16 20:04:07 +0100

    Require Node.js 8 and clean up (#3)

--- test/test.js
+++ test/test.js
@@ -4,7 +4,7 @@ import test from 'ava';
 import cp from 'cp-file';
 import userHome from 'user-home';
 import pify from 'pify';
-import m from '../';
+import resolveAlfredPrefs from '..';
 
 const settings = path.join(userHome, '/Library/Preferences/com.runningwithcrayons.Alfred-Preferences-3.plist');
 
@@ -12,13 +12,13 @@ const mv = (src, dest) => pify(fs.rename)(src, dest).catch(() => { });
 
 test.before(async () => {
 	await mv(settings, `${settings}.back`);
-	await cp('./fixtures/com.runningwithcrayons.Alfred-Preferences-3.plist', settings);
+	await cp(path.join(__dirname, 'fixtures/com.runningwithcrayons.Alfred-Preferences-3.plist'), settings);
 });
 
 test.after(async () => {
 	await mv(`${settings}.back`, settings);
 });
 
-test(async t => {
-	t.is(await m(), path.join(userHome, 'Documents/alfred/Alfred.alfredpreferences'));
+test('resolves `Alfred.alfredpreferences` path', async t => {
+	t.is(await resolveAlfredPrefs(), path.join(userHome, 'Documents/alfred/Alfred.alfredpreferences'));
 });

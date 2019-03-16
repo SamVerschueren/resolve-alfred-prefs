10f38b43eee3069c8cc3ca9c737b4979cb5fc16b
Jakob Krigovsky <jakob@krigovsky.com>
2019-03-16 14:02:51 +0100

    Add test for if the preferences file does not exist

--- test/test.js
+++ test/test.js
@@ -9,16 +9,24 @@ import resolveAlfredPrefs from '..';
 const settings = path.join(userHome, '/Library/Preferences/com.runningwithcrayons.Alfred-Preferences-3.plist');
 
 const mv = (src, dest) => pify(fs.rename)(src, dest).catch(() => { });
+const rm = file => pify(fs.unlink)(file);
 
 test.before(async () => {
 	await mv(settings, `${settings}.back`);
-	await cp(path.join(__dirname, 'fixtures/com.runningwithcrayons.Alfred-Preferences-3.plist'), settings);
 });
 
 test.after(async () => {
 	await mv(`${settings}.back`, settings);
 });
 
-test('resolves `Alfred.alfredpreferences` path', async t => {
+test.serial('resolves `Alfred.alfredpreferences` path', async t => {
+	await cp(path.join(__dirname, 'fixtures/com.runningwithcrayons.Alfred-Preferences-3.plist'), settings);
+
 	t.is(await resolveAlfredPrefs(), path.join(userHome, 'Documents/alfred/Alfred.alfredpreferences'));
+
+	await rm(settings);
+});
+
+test.serial('throws an error if the preferences file does not exist', async t => {
+	await t.throwsAsync(resolveAlfredPrefs, /Alfred preferences not found at location/);
 });
